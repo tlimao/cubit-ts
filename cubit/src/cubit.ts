bf24cf66a792efd6b0237bced9d82d0e89c3bd99
tlimao <tloime@gmail.com>
2024-11-07 17:57:38 -0300

    add method to state stream

--- cubit/src/cubit.ts
+++ cubit/src/cubit.ts
@@ -1,16 +1,22 @@
+import { BehaviorSubject, Observable } from "rxjs";
+
 export class Cubit<T> {
 
-    private _state: T;
+    private _state: BehaviorSubject<T>;
 
     constructor(state: T) {
-        this._state = state;
+        this._state = new BehaviorSubject(state);
     }
 
     public state(): T {
-        return this._state;
+        return this._state.value;
     }
 
-    public emit(state: T) {
-        this._state = state;
+    public emit(state: T): void {
+        this._state.next(state);
+    }
+
+    public stream(): Observable<T> {
+        return this._state.asObservable();
     }
 }
